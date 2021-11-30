b66338caae9f91cd19a01fc026e619e5b95f0ea7
Jacob Schroeder <j.alexander.sch@gmail.com>
2021-11-29 22:54:08 -0500

    Small updates. Debating Nuxtjs or Vuejs for the frontend.

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/gahtoe/pfol
 go 1.17
 
 require (
+	github.com/gofiber/fiber/v2 v2.22.0
+	github.com/gofiber/helmet/v2 v2.2.4
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
-	github.com/gofiber/fiber/v2 v2.22.0 // indirect
-	github.com/gofiber/helmet/v2 v2.2.4 // indirect
 	github.com/klauspost/compress v1.13.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
--- pfol.go
+++ pfol.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/helmet/v2"
 )
 
 func main() {
+	// Create new fiber instance
 	app := fiber.New()
 
+	// Favicon config -- file location of the 'favicon.ico'
 	fc := favicon.Config{
-		File: "./public/assets/favicon.ico",
+		File: "./favicon.ico",
 	}
 
-	app.Use(helmet.New())
+	app.Use(cors.New())      // CORS middleware
+	app.Use(favicon.New(fc)) // Favicon middleware
+	app.Use(helmet.New())    // Helmet middleware
+	app.Use(logger.New())    // Logger middleware
 
-	app.Use(favicon.New(fc))
-
-	app.Static("/", "./public")
-
-	app.Listen(":3000")
+	// Listen on port :3000
+	log.Fatal(app.Listen(":3000"))
 
 }
