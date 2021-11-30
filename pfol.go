f477158b856b4a253eae9b4de7a02e0b440a0021
Jacob Schroeder <j.alexander.sch@gmail.com>
2021-11-30 01:26:12 -0500

    Re-added Vue

--- pfol.go
+++ pfol.go
@@ -5,24 +5,13 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/favicon"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/helmet/v2"
 )
 
 func main() {
 	// Create new fiber instance
 	app := fiber.New()
 
-	// Favicon config -- file location of the 'favicon.ico'
-	fc := favicon.Config{
-		File: "./favicon.ico",
-	}
-
-	app.Use(cors.New())      // CORS middleware
-	app.Use(favicon.New(fc)) // Favicon middleware
-	app.Use(helmet.New())    // Helmet middleware
-	app.Use(logger.New())    // Logger middleware
+	app.Use(cors.New()) // CORS middleware
 
 	// Listen on port :3000
 	log.Fatal(app.Listen(":3000"))
